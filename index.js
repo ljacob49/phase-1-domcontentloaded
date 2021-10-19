8debb560775f56604b8044051e41454813dba48b
Louis Jacob <loujacob2@gmail.com>
2021-10-18 23:13:48 -0400

    commit tree 2a62d0d8987a10325f8c2cdc8ba91322c77fbda8

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function(updateText) {
+   updateText = document.querySelector('#text').innerText("This is really cool")
+});
+    
